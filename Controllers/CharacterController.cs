99cadff935a3b8a114326d7d2acbf603f5471817
joaRojaU <joa.rojasu@duocuc.cl>
2023-05-12 00:06:06 -0400

    Added to string method in rpgClass, added getall

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -11,12 +11,23 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character guard = new Character();
+        private static List<Character> characters = new List<Character>{
+        new Character(),
+        new Character{ Name = "Andrew" }
+
+
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
 
         [HttpGet]
-        public ActionResult<Character> Get()
+        public ActionResult<Character> GetSingle()
         {
-            return Ok(guard);
+            return Ok(characters[0]);
         }
     }
 }
